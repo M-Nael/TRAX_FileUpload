712e6021cf61087f45e99db935b1cb76a7bab732
Mohammed Nael <mohammed.nael@traxgroup.com>
2025-06-11 09:20:28 +0000

    FileUpload: Upload action

---
+++ src/models/CreateLinks.ps1
@@ -0,0 +1,20 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Clear-Host
+$AOSDriveLetter = Read-Host "Please set AOS Service drive letter"
+
+$AOSMetadataPath = $AOSDriveLetter + ":\AOSService\PackagesLocalDirectory"
+#Write-Output $AOSMetadataPath
+
+$RepoModelPath = $PSScriptRoot
+$RepoModelFolders = Get-ChildItem $RepoModelPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+    #Write-Output $ModelFolder
+    
+    $Target = "$RepoModelPath\$ModelFolder"
+	New-Item -ItemType SymbolicLink -Path "$AOSMetadataPath" -Name "$ModelFolder" -Value "$Target"
+}
+
+Read-Host -Prompt "Press any key to continue"
+
+
---
+++ src/models/DeleteLinks.ps1
@@ -0,0 +1,16 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+Clear-Host
+$AOSDriveLetter = Read-Host "Please set AOS Service drive letter"
+$AOSMetadataPath = $AOSDriveLetter + ":\AOSService\PackagesLocalDirectory"
+
+$RepoModelsPath = $PSScriptRoot
+
+$RepoModelFolders = Get-ChildItem $RepoModelsPath -Directory
+foreach ($ModelFolder in $RepoModelFolders)
+{
+	Write-Output "Deleting $AOSMetadataPath\$ModelFolder"
+    #Remove-Item -ItemType SymbolicLink -Path "path_to_symbolic_link"
+	cmd /c rmdir "$AOSMetadataPath\$ModelFolder"
+}
+
+Read-Host -Prompt "Press any key to continue"
